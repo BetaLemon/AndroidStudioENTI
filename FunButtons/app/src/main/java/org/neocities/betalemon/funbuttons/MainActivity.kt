a4dc6f7a3f059881b4d4c56f7b14b63b6494cad1
BetaLemon <alexwelo@gmx.com>
2018-11-06 20:05:31 +0100

    FunButtons sense RecyclerView

---
+++ FunButtons/app/src/main/java/org/neocities/betalemon/funbuttons/MainActivity.kt
@@ -0,0 +1,34 @@
+package org.neocities.betalemon.funbuttons
+
+import android.media.MediaPlayer
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    var mediaPlayer: MediaPlayer? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            //mediaPlayer?.release()
+            mediaPlayer = MediaPlayer.create(this , R.raw.pingas)
+            mediaPlayer?.start()
+        }
+
+        button2.setOnClickListener {
+            //mediaPlayer?.release()
+            mediaPlayer = MediaPlayer.create(this , R.raw.roblox)
+            mediaPlayer?.start()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mediaPlayer?.release()
+        mediaPlayer = null
+    }
+}
